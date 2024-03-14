b120ec80a5e7a94cda9d5395f56511dc9112dd63
Atticus <atticus@macbook36gb.local>
2024-03-13 18:54:47 -0600

    fix(sumu): fix docker file for su and mu

--- servers/mu/entrypoint.sh
+++ servers/mu/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # make a wallet.json file from secrets-manager
-aws secretsmanager get-secret-value --secret-id ao-wallet --query SecretString --output text > wallet.json
+# aws secretsmanager get-secret-value --secret-id ao-wallet --query SecretString --output text > wallet.json
 
 # make a .env file
 echo "PATH_TO_WALLET=wallet.json" >> .env
